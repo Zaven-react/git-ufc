037942194bd85edecdd895596175853b9891136c
Zaven-react <yegoryan@list.ru>
2022-10-10 09:34:09 +0400

    API_axios

--- my-app/src/Components/Fans/Fans.jsx
+++ my-app/src/Components/Fans/Fans.jsx
@@ -1,19 +1,23 @@
+import axios from "axios";
 import React from "react";
-import styles from "./fans.module.css"
+import styles from "./fans.module.css";
+import userPhoto from "../../assets/images/image.png"
+
 
 let Fans =(props)=>{
     if(props.fans.length ===0){
-    
-    props.setFans( [ {id: 1,photoUrl:"https://www.cleveland.com/resizer/vOVEth4p7xAj1zTpWFb2nq0CERU=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SAFGJ36V3VFJTOANC25TCCT4SU.jpg", followed:false,  fullName: "John", status:"lets go",location:{city:"Las Vegas",country:"USA"}},
-                     {id: 2,photoUrl:"https://www.cleveland.com/resizer/vOVEth4p7xAj1zTpWFb2nq0CERU=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SAFGJ36V3VFJTOANC25TCCT4SU.jpg", followed:true, fullName: "Michael", status:"Kill him",location:{city:"London",country:"UK"}},
-                     {id: 3,photoUrl:"https://www.cleveland.com/resizer/vOVEth4p7xAj1zTpWFb2nq0CERU=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SAFGJ36V3VFJTOANC25TCCT4SU.jpg", followed:false, fullName: "Edgar", status:"fuck them",location:{city:"Moscow",country:"Russia"}}])
-                    }
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+           
+            props.setFans(response.data.items)            
+        })   
+
+  }
     return <div>
         {
             props.fans.map(f=> <div key={f.id}>
                 <span>
                     <div>
-                        <img src={f.photoUrl} className={styles.fanPhoto}/>
+                        <img src={f.photos.small !=null?f.photos.small:userPhoto} className={styles.fanPhoto}/>
                     </div>
                     <div>
                         {f.followed?
@@ -24,12 +28,12 @@ let Fans =(props)=>{
                 </span>
                 <span>
                     <span>
-                        <div>{f.fullName}</div>
+                        <div>{f.name}</div>
                         <div>{f.status}</div>
                     </span>
                     <span>                        
-                        <div>{f.location.country}</div>
-                        <div>{f.location.city}</div>
+                        <div>{"f.location.country"}</div>
+                        <div>{"f.location.city"}</div>
                     </span>
                 </span>
             </div>)
