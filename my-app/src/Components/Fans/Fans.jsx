c5635e601fd831045c5eafa8719ac92d96c1fe97
Zaven-react <yegoryan@list.ru>
2022-10-13 08:58:18 +0400

    classComponent

--- my-app/src/Components/Fans/Fans.jsx
+++ my-app/src/Components/Fans/Fans.jsx
@@ -3,26 +3,27 @@ import React from "react";
 import styles from "./fans.module.css";
 import userPhoto from "../../assets/images/image.png"
 
-
-let Fans =(props)=>{
-    if(props.fans.length ===0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
-           
-            props.setFans(response.data.items)            
-        })   
-
-  }
-    return <div>
+class Fans extends React.Component {
+    constructor(props){
+        super(props)       
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{           
+                this.props.setFans(response.data.items)           
+            })         
+    }
+  
+    render(){
+        return <div>
+      
         {
-            props.fans.map(f=> <div key={f.id}>
+            this.props.fans.map(f=> <div key={f.id}>
                 <span>
                     <div>
                         <img src={f.photos.small !=null?f.photos.small:userPhoto} className={styles.fanPhoto}/>
                     </div>
                     <div>
                         {f.followed?
-                        <button onClick={()=>{props.unfollow(f.id)}}>Unfollow</button>:
-                        <button onClick={()=>{props.follow(f.id)}}>Follow</button>}
+                        <button onClick={()=>{this.props.unfollow(f.id)}}>Unfollow</button>:
+                        <button onClick={()=>{this.props.follow(f.id)}}>Follow</button>}
                         
                     </div>
                 </span>
@@ -39,6 +40,7 @@ let Fans =(props)=>{
             </div>)
         }
     </div>
+    }
 }
 
 export default Fans;
